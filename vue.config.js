25bbe60b052118181d4302945547985f419bdc6d
SenpaiVR <SenpaiShichi@gmail.com>
2023-02-14 14:07:22 -0500

    BUILD - update public path

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = defineConfig({
 	transpileDependencies: [
 		'vuetify'
 	],
-	publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+	publicPath: process.env.NODE_ENV === "production" ? "/VRCon-Archive/" : "/",
 })
